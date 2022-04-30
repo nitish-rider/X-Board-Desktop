48b847138d7e75c9be06f770ee476ee7a40d15a6
nitish-rider <nitishks1110@gmail.com>
2022-04-30 16:01:26 +0530

    Device Specific entries made

--- src/main/kotlin/listners/DiscordApp.kt
+++ src/main/kotlin/listners/DiscordApp.kt
@@ -13,7 +13,7 @@ import java.awt.Toolkit
 import java.awt.datatransfer.StringSelection
 import java.util.concurrent.Executors
 
-class DiscordApp(private val otp : String): MessageCreateListener,TyperXView  {
+class DiscordApp(private var otp : String): MessageCreateListener,TyperXView  {
     private val threadPool = Executors.newFixedThreadPool(10)
     private val clipboard = Toolkit.getDefaultToolkit().systemClipboard
     private val typerWorker = TyperXWorker(this)
@@ -21,25 +21,24 @@ class DiscordApp(private val otp : String): MessageCreateListener,TyperXView  {
     override fun onMessageCreate(event: MessageCreateEvent?) {
         if(event != null){
             val message = event.message
-            if(message.content.equals(otp,false)){
-                message.delete()
-                event.channel.sendMessage("Touch the Camera emoji in this message to take a ss")
+            if(message.content.equals(otp,false) && !verification){
+                event.channel.sendMessage("Connected")
                 verification = true
                 channelid = event.channel?.id.toString()
                 println("Verification Successfully on channel $channelid")
             }
-            else if(message.content.length  == 1){
+            else if(message.content.startsWith("#Android->Windows->s", true)){
                 message.delete()
                 takeSS(event.channel)
             }
-            else if(message.content.startsWith("#c", true)){
-                val myString = message.content.substring(2)
+            else if(message.content.startsWith("#Android->Windows->c", true)){
+                val myString = message.content.substring(20)
                 println("Copied $myString into clip board ")
                 val stringSelection = StringSelection(myString)
                 clipboard.setContents(stringSelection, null)
             }
-            else if(message.content.startsWith("#p", true)){
-                val text = message.content.substring(2)
+            else if(message.content.startsWith("#Android->Windows->p", true)){
+                val text = message.content.substring(20)
                 println("Pasting $text into area removing spaces ")
                 typerWorker.isRemoveFrontSpaces = true
                 typerWorker.startRequest(text)
